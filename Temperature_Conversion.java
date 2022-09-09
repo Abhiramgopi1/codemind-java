cec23c80d0a28310457da9d32f21fe8616734eb5
Abhiramgopi1 <96803986+Abhiramgopi1@users.noreply.github.com>
2022-09-09 11:24:07 +0530

    import java.util.*; class temp {     public static void main(String args[])     {         Scanner scan = new Scanner(System.in);         float c = scan.nextFloat();         double f;         f = 32+(c*9/5);         System.out.format("%.2f",f);     }
    }

---
+++ Temperature_Conversion.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class temp
+{
+    public static void main(String args[])
+    {
+        Scanner scan = new Scanner(System.in);
+        float c = scan.nextFloat();
+        double f;
+        f = 32+(c*9/5);
+        System.out.format("%.2f",f);
+    }
+
+}
