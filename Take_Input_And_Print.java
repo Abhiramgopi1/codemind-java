7d9a309052bff679a5e9c49f78a52ac93d109bff
Abhiramgopi1 <96803986+Abhiramgopi1@users.noreply.github.com>
2022-09-16 11:34:49 +0530

    import java.util.*; class print {     public static void main(String args[])     {         Scanner sc = new Scanner(System.in);         String a;         a = sc.nextLine();         System.out.print(a);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class print
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        String a;
+        a = sc.nextLine();
+        System.out.print(a);
+    }
+}
