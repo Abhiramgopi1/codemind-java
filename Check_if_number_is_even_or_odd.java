d21ddccbfcd55cb85a497e2158e62675c29bd9c1
Abhiramgopi1 <96803986+Abhiramgopi1@users.noreply.github.com>
2022-09-18 23:40:49 +0530

    import java.util.*; class CSE {     public static void main(String A[])     {         Scanner scan = new Scanner(System.in);         int a = scan.nextInt();         if(a%2==0)         {             System.out.print("Even");         }         else         {             System.out.print("Odd");         }     } }

---
+++ Check_if_number_is_even_or_odd.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class CSE
+{
+    public static void main(String A[])
+    {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        if(a%2==0)
+        {
+            System.out.print("Even");
+        }
+        else
+        {
+            System.out.print("Odd");
+        }
+    }
+}
