da72d81d7ccbac260055b0894236f590dfd096b0
Abhiramgopi1 <96803986+Abhiramgopi1@users.noreply.github.com>
2022-09-18 23:25:19 +0530

    import java.util.*; class CSE {     public static void main(String A[])     {         Scanner scan = new Scanner(System.in);         int a = scan.nextInt();         int b = scan.nextInt();         scan.close();         if(a%2==0 && (a>0||b%2==0))         {             System.out.print("YES");         }         else         {             System.out.print("NO");         }     } }

--- Can_share_equally?.java
+++ Can_share_equally?.java
@@ -1,12 +1,13 @@
 import java.util.*;
-class sol
+class CSE
 {
-    public static void main(String args[])
+    public static void main(String A[])
     {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        if(a%2==0 && (a>0 || b%2==0))
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        scan.close();
+        if(a%2==0 && (a>0||b%2==0))
         {
             System.out.print("YES");
         }
