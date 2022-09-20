fd42133d29965be292d372c328e746e5396b821a
Abhiramgopi1 <96803986+Abhiramgopi1@users.noreply.github.com>
2022-09-21 00:03:43 +0530

    import java.util.*; class div {     public static void main(String args[])     {         Scanner sc = new Scanner(System.in);         int a = sc.nextInt();         int b = sc.nextInt();         int[] arr = new int[a];         for(int i=0;i<a;i++)         {             arr[i]=sc.nextInt();         }         int c=0;         for(int i=0;i<a;i++)         {             if(arr[i]%b==0)             {                 c++;             }         }         System.out.print(c);     } }

---
+++ Count_elements_divisible_by_k.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class div
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int[] arr = new int[a];
+        for(int i=0;i<a;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        int c=0;
+        for(int i=0;i<a;i++)
+        {
+            if(arr[i]%b==0)
+            {
+                c++;
+            }
+        }
+        System.out.print(c);
+    }
+}
